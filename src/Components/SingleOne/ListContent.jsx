4422da6ce18dcd5d90f18fad256bfa0565b92314
trdmitr <trdmitr@gmail.com>
2022-12-22 10:04:03 +0400

    up videoblock

--- src/Components/SingleOne/ListContent.jsx
+++ src/Components/SingleOne/ListContent.jsx
@@ -24,13 +24,17 @@ const ListContent = () => {
     const params = useParams();
     const { status, data: songs, isFetching, error } = useQuery(['639c448d01a72b59f2321459'], fetchPost, { staleTime: 60000 }, { cacheTime: 1000 * 60 * 60 }, { refetchOnWindowFocus: false }, { enabled: false }, { retry: 1 });
     if (status === 'loading') {
-        return <RoundLoader/>
+        return <RoundLoader />
     }
 
     if (status === 'error') {
         return <h1 style={{ backgroundColor: "white", color: "black" }}>Ошибка загрузки {error.message}</h1>
     }
-    const currSings = songs.record.cavers.filter(songs => songs.id == params.id);
+    const currSings = songs.record.cavers.filter(songs => songs.id === params.id);
+    const classeNam = (x, y) => {
+        if (x === y)
+            return classes.mediaHidden;
+    }
     return (
 
         currSings.map((currSing) =>
@@ -41,23 +45,6 @@ const ListContent = () => {
                     <h2>{currSing.name}</h2></div>
                 <a className={[classes.linkTo, currSing.linkTo.trim() ? '' : classes.mediaHidden].join(' ')} href={currSing.linkTo} target="_blank" rel="noopener noreferrer"> Канал исполнителя </a>
                 <div className={[
-                    classes.audioBlock,
-                    currSing.audio1.trim() ? null : classes.mediaHidden].join(' ')
-                }>
-                    <p>{currSing.audio_name1}</p>
-                    <audio controls className={currSing.audio1.trim() ? null : classes.mediaHidden}
-                        src={currSing.audio1} type="audio/mpeg" />
-                    <p>{currSing.audio_name2}</p>
-                    <audio controls className={currSing.audio2.trim() ? null : classes.mediaHidden}
-                         src={currSing.audio2} type="audio/mpeg" />
-                    <p>{currSing.audio_name3}</p>
-                    <audio controls className={currSing.audio3.trim() ? null : classes.mediaHidden}
-                         src={currSing.audio3} type="audio/mpeg" />
-                </div>
-                <div className={[classes.tziTata, currSing.picture_tzitata.trim() ? null : classes.mediaHidden].join(' ')}>
-                    
-                </div>
-                <div className={[
                     classes.videoBlock,
                     currSing.video1.trim() ? null : classes.mediaHidden].join(' ')
                 }>
@@ -65,7 +52,9 @@ const ListContent = () => {
                     {/* <ReactPlayer className={currSing.video1 ? '': classes.mediaHidden}
                     id= {classes.videoFrame} url = {currSing.video1} id= {classes.videoFrame}  /> */}
                     <p>{currSing.video_name1}</p>
-                    <video className={currSing.video1.trim() ? null : classes.mediaHidden} src={currSing.video1} id={classes.videoFrame} controls={true} type="video/mp4" ></video>
+                    <video className={currSing.video1.trim() ? null
+                        : classes.mediaHidden} src={currSing.video1}
+                        id={classes.videoFrame} controls={true} type="video/mp4" ></video>
                     <p>{currSing.video_name2}</p>
                     {/* <div className={[classes.videoEdge, currSing.video1.trim() ? null : classes.mediaHidden].join(' ')} >
                      <iframe width="100%" height="100%" src={currSing.video1}  frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>    
@@ -76,9 +65,27 @@ const ListContent = () => {
                     {/* <ReactPlayer className={currSing.video3 ? '': classes.mediaHidden} id={classes.videoFrame} url = {currSing.video3} controls={true} /> */}
                     {/* <video className={currSing.video3 ? '': classes.mediaHidden} src={currSing.video3} id = {classes.videoFrame} controls={true} ></video> */}
                     <video className={currSing.video3.trim() ? null : classes.mediaHidden} src={currSing.video3} id={classes.videoFrame} controls={true} type="video/mp4" ></video>
-                    <img className={classes.tziImage} src={currSing.picture_tzitata} width={80} alt="Цитаты" />
 
                 </div>
+                <div className={[
+                    classes.audioBlock,
+                    currSing.audio1.trim() ? null : classes.mediaHidden].join(' ')
+                }>
+                    <p>{currSing.audio_name1}</p>
+                    <audio controls className={currSing.audio1.trim() ? null
+                        : classes.mediaHidden}
+                        src={currSing.audio1} type="audio/mpeg" />
+                    <p>{currSing.audio_name2}</p>
+                    <audio controls className={currSing.audio2.trim() ? null : classes.mediaHidden}
+                        src={currSing.audio2} type="audio/mpeg" />
+                    <p>{currSing.audio_name3}</p>
+                    <audio controls className={currSing.audio3.trim() ? null : classes.mediaHidden}
+                        src={currSing.audio3} type="audio/mpeg" />
+                </div>
+                <div className={[classes.tziTata, currSing.picture_tzitata.trim() ? null : classes.mediaHidden].join(' ')}>
+                    <img className={classes.tziImage} src={currSing.picture_tzitata} width={80} alt="Цитаты" />
+                </div>
+
             </div>
         )
     );
